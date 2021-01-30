9be9a03dd6a9a79fe4c271eb01b7b07fa4c3669d
jayps <jaypsaade@gmail.com>
2021-01-30 19:03:28 +0200

    [CHORE] Added constants for code usage

---
+++ src/constants/errors.ts
@@ -0,0 +1,7 @@
+export const Errors = {
+    SomeError: { code: 'SOME_ERROR', message: '{{message}}' },
+    UnexpectedError: {
+        code: 'UNEXPECTED_ERROR',
+        message: 'An unexpected error has occurred.'
+    }
+};
