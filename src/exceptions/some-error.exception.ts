b7eebfe1d13dd5031e244b94fbe079c3c2a353b8
jayps <jaypsaade@gmail.com>
2021-01-30 19:18:17 +0200

    [FIX] changed className

--- src/exceptions/some-error.exception.ts
+++ src/exceptions/some-error.exception.ts
@@ -2,11 +2,11 @@ import { Errors } from '../constants/errors';
 import { StatusCodes } from '../constants/statusCodes';
 import HttpException from './http.exception';
 
-class UnexpectedException extends HttpException {
+class SomeException extends HttpException {
     constructor(message: string) {
         const error = Errors.SomeError;
         super(StatusCodes.HandledError, error.code, error.message.replace(`{{message}}`, message));
     }
 }
 
-export default UnexpectedException;
+export default SomeException;
